fa2d7657d7b0770ac5819711b30d4808609d8490
Inder <kumarinder799@gmail.com>
2026-01-12 00:38:33 +0530

    Single Hotel Router

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./config/dbconfig.js";
 import hotelDataAddedToDBRouter from "./routes/dataimport.router.js";
 import categoryAddedToDBRouter from "./routes/categoryimport.router.js";
 import categoryRouter from "./routes/category.router.js";
+import singleHotelRouter from "./routes/singlehotel.router.js";
 
 const app = express();
 app.use(express.json());
@@ -23,6 +24,8 @@ app.use("/api/categorydata", categoryAddedToDBRouter);
 app.use("/api/hotels", hotelRouter);
 
 app.use("/api/category", categoryRouter);
+
+app.use("/api/hotels", singleHotelRouter);
 
 mongoose.connection.once("open", () => {
   console.log("Connected to DB");
